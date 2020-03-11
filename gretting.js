aad182e5083bac7100354bb6c48870fbe6c09d51
unknown <cyberesident@naver.com>
2020-03-11 18:57:54 +0900

    ~3.3

---
+++ gretting.js
@@ -0,0 +1,27 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
+
+
+const USER_LS = "currentUser";
+    SHOWING_ON = "showing";
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_ON);
+    greeting.classList.add(SHOWING_ON);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null){
+
+    } else {
+        paintGreeting(currentUser);
+    }
+}
+
+function init(){
+    loadName();
+}
+
+init();
